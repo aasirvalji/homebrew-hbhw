e10b8d212ed39796a6167c5fa08aa768f634d0fb
aasirvalji <aasir.valji@hotmail.com>
2022-01-10 21:11:48 -0500

    added 0.0.4 ref

--- hbhw.rb
+++ hbhw.rb
@@ -1,8 +1,8 @@
 class Hbhw < Formula
   desc "Prints hello world and new version"
   homepage "https://github.com/aasirvalji/hbhw"
-  url "https://github.com/aasirvalji/hbhw/archive/refs/tags/v0.0.3.tar.gz"
-  sha256 "c9a683fdfdb54bdfbaa5a8242d7c3687a08839d4485050e596fcf1740bab1f76"
+  url "https://github.com/aasirvalji/hbhw/archive/refs/tags/v0.0.4.tar.gz"
+  sha256 "1f48ac17ea5b64e8a222553f169737622eee25f9be881b633f625742bdf9a79e"
   license "MIT"
 
   def install
